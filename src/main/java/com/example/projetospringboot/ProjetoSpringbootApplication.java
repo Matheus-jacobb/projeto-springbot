e17ceacc5ab48a2bd43801942edc8dfa7b99b753
Matheus <matheusbendel@hotmail.com>
2020-09-08 19:56:42 -0300

    Meu primeiro commit

---
+++ src/main/java/com/example/projetospringboot/ProjetoSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.projetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootApplication.class, args);
+	}
+}
